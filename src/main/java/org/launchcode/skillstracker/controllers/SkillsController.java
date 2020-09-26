ffeb6a9a0f670e0c027c9c2538af538e5a4fbea0
elishabaii <eliernst@sbcglobal.net>
2020-09-26 18:01:19 -0500

    Finished studio

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -2,31 +2,63 @@ package org.launchcode.skillstracker.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SkillsController {
 
-    @GetMapping("form")
+    @GetMapping()
     @ResponseBody
-    public String skillsForm(){
-        return"<h1>Skills Tracker</h1>" +
+    public String skillsList() {
+        return "<h1>Skills Tracker</h1>" +
                 "<h2> These are some skills I'd like to learn</h2>" +
                 "<ol>" +
                 "<li>CSS</li>" +
                 "<li>Java</li>" +
                 "<li>html</li>" +
                 "</ol>";
+    }
 
-//        return "<html>" +
-//                "<body>" +
-//                "<form>" +
-//                "<input type = 'text' skill='skill'>" +
-//                "<input type='submit' value = 'Skill'>" +
-//                "</form>" +
-//                "</body>" +
-//                "</html>";
+    @GetMapping("form")
+    @ResponseBody
+    public String skillForm(){
 
+        return "<form action = 'form' method = 'post'>" +
+                "<input type= 'text' name ='name'><br />" +
+                "<label>Zeroth Favorite</label><br />" +
+                "<select name ='skill0'>" +
+                "<option value ='Java'>Java</option>" +
+                "<option value = 'Python'>Python</option>" +
+                "<option value = 'Javascript'>Javascript</option>" +
+                "</select><br />" +
+                "<label>First favorite</label><br />" +
+                "<select name ='skill1'>" +
+                "<option value ='Java'>Java</option>" +
+                "<option value = 'Python'>Python</option>" +
+                "<option value = 'Javascript'>Javascript</option>" +
+                "</select><br />" +
+                "<label>Third</label><br />" +
+                "<select name ='skill2'>" +
+                "<option value ='Java'>Java</option>" +
+                "<option value = 'Python'>Python</option>" +
+                "<option value = 'Javascript'>Javascript</option>" +
+                "</select><br />" +
+                "<input type='submit' value = 'Skill'>" +
+                  "</form>";
+
+
+    }
+    @PostMapping("form")
+    @ResponseBody
+    public String processForm(@RequestParam String name, @RequestParam String skill0, @RequestParam String skill1, String skill2){
+        return "<h1>" + name + "</h1> <br/>" +
+                "<ol>" +
+                "<li> " + skill0 + "</li>" +
+                "<li> " + skill1 + "</li>" +
+                "<li> " + skill2 + "</li>" +
+                "</ol>";
     }
 
 }
